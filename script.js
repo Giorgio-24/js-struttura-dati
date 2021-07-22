941b2e37923abc32f6d333256e3d3c5eff80a75e
Giorgio Andrei <cipuandrei@libero.it>
2021-07-22 15:20:18 +0200

    card-objects-created

---
+++ script.js
@@ -0,0 +1,36 @@
+var magicCard = {
+    id: 1,
+    name: 'Bloodfire Colossus',
+    launchCost: ['6', 'R', 'R'],
+    combninedLaunchCost: 8,
+    cardType: 'creature',
+    subType: 'giant',
+    expansion: {
+        reprintId: 9,
+        expansionName: 'Exodus',
+        rarityColor: 'silver',
+    },
+    flavorText: {
+        quote: 'bla bla bla...',
+        author: 'Lochness Monster',
+    },
+    abilities: [
+        {
+            launchCost: ['R', 'T'],
+            description: '...alb alb alb',
+        },
+        {//^IN CASE OF OTHER ABILITIES.
+            launchCost: ['U', 'T'],
+            description: 'some random text',
+        },
+    ],
+    collectionNumber: '177/350',
+    constitution: 13,
+    strength: 13,
+    cardBorderColor: '#f00',
+    illustration: {
+        id: 1,
+        name: 'Greg Staples',
+    },
+};
+
